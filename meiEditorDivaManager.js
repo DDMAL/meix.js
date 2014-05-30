ddb55fc7a4d9164cf8a5ea670e84d28b2440038f
Andrew Horwitz <ahwitz@gmail.com>
2014-05-30 13:40:36 -0400

    New: halfway done with GUI display for linking pages
    Looking into storage solutions now, just saving my work here.

--- meiEditorDivaManager.js
+++ meiEditorDivaManager.js
@@ -11,26 +11,23 @@ var meiEditorDivaManager = function()
             + 'Unlinked files in Diva:<br>'
             + '<div id="diva-file-list"></div>'
             + '</div>'
+            + '<div id="diva-manager-button-container"><button id="link-files">Link selected files</button><div id="diva-manager-error"></div></div>'
             + '<div id="linked-file-header">Linked files:</div>'
-            + '<div id="linked-file-list"></div>'
-            + '<button id="linkFiles">Link files</button>',
+            + '<div id="linked-file-list"></div>',
         minimizedTitle: 'Diva page manager:',
         minimizedAppearance: '',
         _init: function(meiEditor, meiEditorSettings)
         {
             $.extend(meiEditorSettings, {
                 divaPageList: [],
+                meiFilesToDivaImages: {},
             });
 
-            meiEditor.newFunction = function()
-            {
-                thisFunctionIsOnlyLocal();
-            }
-            meiEditor.events.subscribe("NewFile", function(fileData, fileName, fileNameOriginal){
+            meiEditor.events.subscribe("NewFile", function(fileData, fileNameStripped, fileNameOriginal){
                 $("#manager-file-list").html($("#manager-file-list").html()
-                    + "<div class='meiFile' pageTitle='" + fileName + "' id='validate-" + fileName + "'>" + fileNameOriginal
+                    + "<div class='meiFile' pageTitle='" + fileNameStripped + "' id='unlinked-mei-" + fileNameStripped + "'>" + fileNameOriginal
                     + "<span class='meiFileButtons'>"
-                    + "<input type='radio' name='manager-files' value='" + fileName + "'>"
+                    + "<input type='radio' name='manager-files' strippedPage='" + fileNameStripped + "' value='" + fileNameStripped + "'>"
                     + "</span>"
                     + "</div>");
             });
@@ -42,18 +39,34 @@ var meiEditorDivaManager = function()
                 success: function (data, status, jqxhr)
                 {
                     for(curPage in data.pgs){
-                        curPageFileName = data.pgs[curPage].f;
-                        fileNameStripped = curPageFileName.replace(/\W+/g, "");
-                        meiEditorSettings.divaPageList.push(curPageFileName);
+                        fileNameOriginal = data.pgs[curPage].f;
+                        fileNameStripped = fileNameOriginal.replace(/\W+/g, "");
+                        meiEditorSettings.divaPageList.push(fileNameOriginal);
                         $("#diva-file-list").html($("#diva-file-list").html()
-                            + "<div class='meiFile' pageTitle='" + fileNameStripped + "' id='validate-" + fileNameStripped + "'>" 
+                            + "<div class='meiFile' pageTitle='" + fileNameOriginal + "' id='unlinked-diva-" + fileNameStripped + "'>" 
                             + "<span class='meiFileButtons' style='float:left;'>"
-                            + "<input type='radio' name='manager-files' value='" + fileNameStripped + "'>"
+                            + "<input type='radio' name='diva-images' strippedPage='" + fileNameStripped + "' value='" + fileNameOriginal + "'>"
                             + "</span>"
-                            + curPageFileName
+                            + fileNameOriginal
                             + "</div>");
                     }
                 }
+            });
+
+            $("#link-files").on('click', function(){
+                $("#diva-manager-error").html("");
+                var selectedMEI = $('input[name=manager-files]:checked').val();
+                var selectedImage = $('input[name=diva-images]:checked').val();
+                var selectedStrippedMEI = $('input[name=manager-files]:checked').attr('strippedPage');
+                var selectedStrippedImage = $('input[name=diva-images]:checked').attr('strippedPage');
+                if(selectedMEI === undefined || selectedImage === undefined)
+                {
+                    $("#diva-manager-error").html("Please make sure that an MEI file and an image are selected.");
+                    return;
+                }
+                meiEditorSettings.meiFilesToDivaImages[selectedImage] = selectedMEI;
+                $("#unlinked-mei-"+selectedStrippedMEI).remove();
+                $("#unlinked-diva-"+selectedStrippedImage).remove();
             });
         }
     }
