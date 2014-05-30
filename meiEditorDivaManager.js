0e1c26c8905341bc5328f8d403d3525d4861a780
Andrew Horwitz <ahwitz@gmail.com>
2014-05-30 15:48:14 -0400

    New: linking and unlinking files reflected in GUI
    buuut not much else yet
    
    also #coolScrollbarTricks

--- meiEditorDivaManager.js
+++ meiEditorDivaManager.js
@@ -25,11 +25,12 @@ var meiEditorDivaManager = function()
 
             meiEditor.events.subscribe("NewFile", function(fileData, fileNameStripped, fileNameOriginal){
                 $("#manager-file-list").html($("#manager-file-list").html()
-                    + "<div class='meiFile' pageTitle='" + fileNameStripped + "' id='unlinked-mei-" + fileNameStripped + "'>" + fileNameOriginal
-                    + "<span class='meiFileButtons'>"
-                    + "<input type='radio' name='manager-files' strippedPage='" + fileNameStripped + "' value='" + fileNameStripped + "'>"
+                    + "<div class='meiFileWrapper'>"
+                    + "<div class='meiFile' pageTitle='" + fileNameStripped + "' id='unlinked-mei-" + fileNameStripped + "' style='display:inline-block;'>" + fileNameOriginal
+                    + "<span class='linkRadioButtons'>"
+                    + "<input type='radio' name='manager-files' strippedPage='" + fileNameStripped + "' value='" + fileNameOriginal + "'>"
                     + "</span>"
-                    + "</div>");
+                    + "</div><br></div>");
             });
 
             $.ajax({
@@ -43,12 +44,13 @@ var meiEditorDivaManager = function()
                         fileNameStripped = fileNameOriginal.replace(/\W+/g, "");
                         meiEditorSettings.divaPageList.push(fileNameOriginal);
                         $("#diva-file-list").html($("#diva-file-list").html()
-                            + "<div class='meiFile' pageTitle='" + fileNameOriginal + "' id='unlinked-diva-" + fileNameStripped + "'>" 
-                            + "<span class='meiFileButtons' style='float:left;'>"
+                            + "<div class='meiFileWrapper'>"
+                            + "<div class='meiFile' pageTitle='" + fileNameOriginal + "' id='unlinked-diva-" + fileNameStripped + "' style='display:inline-block;'>" 
+                            + "<span class='linkRadioButtons' style='float:left;'>"
                             + "<input type='radio' name='diva-images' strippedPage='" + fileNameStripped + "' value='" + fileNameOriginal + "'>"
                             + "</span>"
                             + fileNameOriginal
-                            + "</div>");
+                            + "</div><br></div>");
                     }
                 }
             });
@@ -65,8 +67,29 @@ var meiEditorDivaManager = function()
                     return;
                 }
                 meiEditorSettings.meiFilesToDivaImages[selectedImage] = selectedMEI;
-                $("#unlinked-mei-"+selectedStrippedMEI).remove();
-                $("#unlinked-diva-"+selectedStrippedImage).remove();
+                $("#unlinked-mei-"+selectedStrippedMEI).parent().css('display', 'none');
+                $("#unlinked-diva-"+selectedStrippedImage).parent().css('display', 'none');
+                $("#linked-file-list").html($("#linked-file-list").html()
+                    + "<div class='linkedMeiWrapper'>"
+                    + "<span class='meiFile' id='linked-" + selectedStrippedMEI + "'>" + selectedMEI + "</span>"
+                    + "<span class='meiFile' id='linked-" + selectedStrippedImage + "'>" + selectedImage + "</span>"
+                    + "<button class='unlink'>Unlink</button>"
+                    + "</div>");
+                $(".unlink").on('click', function()
+                {
+                    var numberOfChildren = $(this).parent().children("span").length;
+                    while(numberOfChildren--)
+                    {
+                        curChild = $(this).parent().children("span")[numberOfChildren];
+                        console.log(curChild);
+                        var tempID = curChild.id.split("-")[1];
+                        console.log(tempID);
+                        $("#unlinked-mei-"+tempID).parent().css('display', 'inline-block');
+                        $("#unlinked-diva-"+tempID).parent().css('display', 'inline-block');
+                        $("#linked"+tempID).remove();
+                    }
+                    $(this).parent().remove()
+                });
             });
         }
     }
